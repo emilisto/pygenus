99f6bcb4f4fca0950a29672927e3823d90299f04
Jānis Ābele <janis.abele@gmail.com>
2014-07-26 15:07:55 +0300

    Fix print statement for python 3

--- experiments/postags.py
+++ experiments/postags.py
@@ -1,8 +1,10 @@
 import sys
 from pprint import pprint
+
 import nltk
 
-print "Reading from stdin..."
+
+print('Reading from stdin...')
 raw = sys.stdin.read()
 tokenized = nltk.word_tokenize(raw)
 postags = nltk.pos_tag(tokenized)
