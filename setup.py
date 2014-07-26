c635c15f0bafa580458359b4bac87871ba942aac
Emil Stenqvist <emsten@gmail.com>
2014-07-26 14:17:51 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygenus',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Emil Stenqvist and Janis Abele',
+    description='A work-in-progress Python module for identifying mentions of'
+                'gender in text using natural language processing.',
+)
