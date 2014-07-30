4da2b5a41bfdd7c22cf9bb7b5da3460398f3468f
Jānis Ābele <janis.abele@gmail.com>
2014-07-30 21:33:53 +0300

    Remove all special cases and use only classifier

--- pygenus.py
+++ pygenus.py
@@ -9,6 +9,9 @@ MALE_PRONOUN_SEQ = ('he', 'him', 'his', 'himself')
 FEMALE_PRONOUN_SEQ = ('she', 'her', 'hers', 'herself')
 NEUTRAL_PRONOUN_SEQ = ('they', 'them', 'their', 'theirs', 'themselves')
 
+CLASIFIER_CACHE = None
+"""Cache trained classifier."""
+
 
 class Classifier:
 
@@ -16,35 +19,15 @@ class Classifier:
         self.classifier = classifier
 
     def classify(self, text):
-        # Here goes all data preprocessing
-        tagged_words = nltk.pos_tag(nltk.word_tokenize(text))
-        words = [
-            word for word, tag in
-            filter(is_possible_gender_word, tagged_words)
-        ]
+        pos_tag_list = nltk.pos_tag(nltk.word_tokenize(text))
 
-        for word in words:
-            tag = self.classify_word(word)
-            if tag != 'neutral':
-                yield word, self.classify_word(word)
+        for pos_tag in pos_tag_list:
+            classification = self.classify_word(pos_tag)
+            if classification != 'neutral':
+                yield pos_tag[0], classification
 
-    def classify_word(self, word):
-        return self.classifier.classify(_gender_features(word))
-
-
-def is_possible_gender_word(wordtag):
-    _, tag = wordtag
-    # TODO: narrow down the word classes as much as possible - possibly
-    # include NNP and NNS instead of NN
-    desired_tags = ('PRP', 'NNS', 'NNP')
-    return any(tag.startswith(desired_tag) for desired_tag in desired_tags)
-
-
-def _gender_features(word):
-    if word.lower() in MALE_PRONOUN_SEQ + FEMALE_PRONOUN_SEQ + NEUTRAL_PRONOUN_SEQ:
-        return {'pronoun': word.lower()}
-    else:
-        return {'last_letters': word[-2:]}
+    def classify_word(self, pos_tag):
+        return self.classifier.classify(_gender_features(pos_tag))
 
 
 def classify(text):
@@ -54,26 +37,50 @@ def classify(text):
 
 def new_naive_bayes_classifier():
     # Create featureset consiting of male and female names for training
-    male_word_seq = (
-        (name, 'male')
-        for name in chain(names.words('male.txt'), MALE_PRONOUN_SEQ))
-    female_word_seq = (
-        (name, 'female')
-        for name in chain(names.words('female.txt'), FEMALE_PRONOUN_SEQ))
-    neutral_pronoun_seq = (
-        (pronoun, 'neutral')
-        for pronoun in NEUTRAL_PRONOUN_SEQ)
+    global CLASIFIER_CACHE
+    if CLASIFIER_CACHE:
+        return CLASIFIER_CACHE
+    else:
+        male_word_seq = (
+            ((name, tag), 'male')
+            for name, tag in nltk.pos_tag(
+                    list(chain(names.words('male.txt'), MALE_PRONOUN_SEQ))))
+        female_word_seq = (
+            ((name, tag), 'female')
+            for name, tag in nltk.pos_tag(
+                    list(chain(names.words('female.txt'), FEMALE_PRONOUN_SEQ))))
+        neutral_pronoun_seq = (
+            ((pronoun, tag), 'neutral') for pronoun, tag in nltk.pos_tag(
+                NEUTRAL_PRONOUN_SEQ))
 
-    featureset_seq = (
-        (_gender_features(word), gender)
-        for word, gender in chain(
-                male_word_seq,
-                female_word_seq,
-                neutral_pronoun_seq,
-        ))
+        featureset_seq = (
+            (_gender_features(word), gender)
+            for word, gender in chain(
+                    male_word_seq,
+                    female_word_seq,
+                    neutral_pronoun_seq,
+            ))
+        CLASIFIER_CACHE = nltk.NaiveBayesClassifier.train(featureset_seq)
 
-    # Train and return ready classifier
-    return nltk.NaiveBayesClassifier.train(featureset_seq)
+        return CLASIFIER_CACHE
+
+
+def _gender_features(pos_tag):
+    word_raw, tag = pos_tag
+    word = _normalize_word(word_raw)
+
+    feature_dict = {'pos_tag': tag}
+
+    if word in MALE_PRONOUN_SEQ + FEMALE_PRONOUN_SEQ + NEUTRAL_PRONOUN_SEQ:
+        feature_dict = {'pronoun': word.lower()}
+    else:
+        feature_dict.update({'last_letters': word[-2:]})
+
+    return feature_dict
+
+
+def _normalize_word(word_raw):
+    return word_raw.lower()
 
 
 def main():
